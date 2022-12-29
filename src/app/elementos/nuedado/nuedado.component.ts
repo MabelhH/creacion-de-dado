91bb8d39894f6b6756a8eecfeb40844920fab099
clara23 <clarahuanaco20@gmail.com>
2022-12-29 11:43:24 -0500

    nuevos cambios

--- src/app/elementos/nuedado/nuedado.component.ts
+++ src/app/elementos/nuedado/nuedado.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./nuedado.component.css']
 })
 export class NuedadoComponent {
+
 numero:number=0;
+numero2:number=0;
+
+tirardado(){
+ this.numero=(Math.floor(Math.random()*10)+1)
+ let x=7
+ while( x>6 || x==this.numero2 ){
+  this.numero=(Math.floor(Math.random()*10)+1)
+  x=this.numero
+ }
+ this.numero2=this.numero
+
+
+ 
+ 
+}
 }
